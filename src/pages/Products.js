2f3f8d0ac4c81528b7c4a2640300d9a0ca782b7b
Ann Chemutai <annchemu@gmail.com>
2021-12-06 16:54:31 +0300

    updates

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,14 +5,10 @@ import { useQuery, gql } from "@apollo/client";
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -22,6 +18,9 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import IconButton from '@mui/material/IconButton';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import CloseIcon from '@mui/icons-material/Close';
 
 const ProductsQuery = gql`
     query Product($currency:Currency){
@@ -40,17 +39,29 @@ const CurrencyListQuery = gql`
   }
     
 `;
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
   }));
 
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-  });
+const styles = 
+{
+    media: {
+        height: 140,
+        paddingTop: '15.25%', // 16:9,
+        marginTop:'10',
+        objectFit: 'contain'
+    },
+    button: {
+        backgroundColor: '#4b5548'
+    }
+};
+const drawerWidth = 500;
 
 const Products = () => {
     
@@ -59,16 +70,14 @@ const Products = () => {
     const [open, setOpen] = React.useState(false);
     const [currency, setCurrency] = useState('USD')
     const ProductsData = useQuery(ProductsQuery,{ variables: {currency}});
-    // const ProductsData = useQuery(ProductsQuery);
     const CurrencyData= useQuery(CurrencyListQuery);
     const error = ProductsData.error || CurrencyData.error;
     const loading = ProductsData.loading || CurrencyData.loading;
+    
 
     useEffect(() => {
         document.title = 'All Products|Lumin Skincare';
-
         total();
-        // console.log(currencylist)
     }, [cart]);
 
     const total = () => {
@@ -80,16 +89,19 @@ const Products = () => {
       };
 
     const addToCart = (e) => {
+        
         setCart([...cart, e]);
             console.log('The product has been added to cart:', cart);
         setOpen(true);
 
-        // const check_index = cart.find(item => item.id === e.id);
-        // console.log(check_index.id);
-        // if (check_index !== -1) {
-        //     // cart[check_index].quantity++;
-        //     console.log("Quantity updated:", cart);
-        // } else {
+        // const check_item = cart.findIndex(item => item.id === e.id);
+        // console.log(check_item.id);
+        // if (check_item !== -1) {
+        //     cart.quantity = 0
+        //    cart[check_index].quantity++;
+        //     console.log("Quantity updated:");
+        // } 
+        // else {
         //     setCart([...cart, e]);
         //     console.log('The product has been added to cart:', cart);
         // }
@@ -107,43 +119,44 @@ const Products = () => {
     };
 
     if (loading) {
-        return <div>loading</div>;
+        return <div>{loading}</div>;
     }
 
     if (error) {
     return <div>{error}</div>;
     }
     
-      const handleClose = () => {
-        setOpen(false);
-      };
+    const handleClose = () => {
+    setOpen(false);
+    };
 
     return (
         <div className="App">
             <Box sx={{ width: '100%',  backgroundColor: '#e0e2e0' }} justifyContent="center"  alignItems="center" >
-            <Grid sx={{ flexGrow: 1 }} container spacing={2}>
-            <AppBar position="static" sx={{backgroundColor: '#e0e2e0'}}>
+                
+                <AppBar position="fixed" sx={{backgroundColor: '#e0e2e0'}}>
                     <Toolbar>
-                        <Typography variant="h6" color="#000000" component="div" sx={{ flexGrow: 1 }}>
+                        <Typography variant="h6" color="#000" align='left' component="div" sx={{ flexGrow: 1 }}>
                             L  U  M  I  N
                         </Typography>
                         <Button color="inherit">Shop</Button>
                         <Button color="inherit">Learn</Button>
                         <Button color="inherit">Account</Button>
-                        <Button color="inherit">Cart({cart.length})</Button>
+                        <Button color="inherit" onClick={() => setOpen(true)}>Cart({cart.length})</Button>
                     </Toolbar>
                 </AppBar>
+                <Grid sx={{ flexGrow: 1 }} container spacing={2}>
                 <Grid xs={2} item></Grid>
                 <Grid xs={8} item >
-                <Grid container justifyContent="center" rowSpacing={4} columnSpacing={{ xs: 2, sm: 3, md: 4 }}>
+                <Grid container rowSpacing={4} columnSpacing={{ xs: 2, sm: 3, md: 4 }}>
                 {ProductsData.data.products.map((product) => (
-                <Grid item xs={4} item key={product.id}>
+                <Grid item xs={4} key={product.id}>
                     <Card >
                     <CardMedia
                         component="img"
-                        height="140"
                         image={product.image_url}
                         alt={product.title}
+                        style={styles.media}
                     />
                     <CardContent>
                         <Typography gutterBottom variant="body2" component="div">
@@ -154,7 +167,7 @@ const Products = () => {
                         </Typography>
                     </CardContent>
                     <CardActions>
-                        <Button variant='contained' onClick={() => addToCart(product)}>Add to Cart</Button>
+                        <Button variant='contained' style={styles.button} onClick={() => addToCart(product)}>Add to Cart</Button>
                     </CardActions>
                 </Card>
                 </Grid>
@@ -163,33 +176,50 @@ const Products = () => {
             </Grid>
             <Grid xs={2} item></Grid>
                     {/* Cart Items */}
-             <Dialog
-                open={open}
-                TransitionComponent={Transition}
-                keepMounted
-                onClose={handleClose}
-                aria-describedby="alert-dialog-slide-description"
-            >
-                <DialogTitle>My Shopping Cart
-                    <Select
-                        value={currency}
-                        onChange={handleChange}
-                        >
-                            {CurrencyData.data.currency.map(cl =>(
-                                <MenuItem value={cl}>{cl}</MenuItem>
-                            ))}
-                    </Select>
-                    <Button onClick={handleClose}>X</Button>
-                </DialogTitle>
-                <DialogContent>
+             
+            <Box>
+            <Drawer
+                    sx={{
+                        width: drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        backgroundColor: '#f5f5f4',
+                        overflow: 'auto'
+                        },
+                    }}
+                    anchor='right'
+                    open = {open}
+                    onClose={handleClose}
+                >
+                <Grid container spacing={2}>
+                    <DrawerHeader>
+                        <Typography variant="subtitle1">My Shopping Cart </Typography>
+                        
+                        <IconButton onClick={handleClose}>
+                            <ChevronRightIcon />
+                        </IconButton>
+                    
+                        <Select
+                            value={currency}
+                            onChange={handleChange}
+                            >
+                                {CurrencyData.data.currency.map(cl =>(
+                                    <MenuItem key={cl} value={cl}>{cl}</MenuItem>
+                                ))}
+                        </Select>
+                    </DrawerHeader>
+                </Grid>
                     {cart.map((item) => (
                     <Card sx={{ maxWidth: 345 }}>
-                        <Button onClick={() => removeFromCart(item)}>x</Button>
+                        <IconButton edge='end' onClick={() => removeFromCart(item)}>
+                            <CloseIcon />
+                        </IconButton>
                         <CardMedia
                             component="img"
-                            height="140"
                             image={item.image_url}
                             alt={item.title}
+                            style={styles.media}
                         />
                         <CardContent>
                             <Typography gutterBottom variant="body2" component="div">
@@ -198,23 +228,21 @@ const Products = () => {
                             
                         </CardContent>
                         <CardActions>
-                            <ButtonGroup variant="outlined" aria-label="outlined button group">
+                            <ButtonGroup variant="outlined"  aria-label="outlined button group">
                                 <Button>+</Button>
                                 <Button>1</Button>
                                 <Button>-</Button>
                             </ButtonGroup>
                             <Typography variant="body2" color="text.secondary">
-                            {item.price}
+                            &nbsp;&nbsp;&nbsp;{currency} {item.price}
                             </Typography>
                         </CardActions>
                     </Card>
                     ))}
-                </DialogContent>
-                <DialogActions>
-                <div>SUBTOTAL: {currency} {cartTotal}</div>
-                <Button onClick={handleClose}>PROCEED TO CHECKOUT</Button>
-                </DialogActions>
-            </Dialog>
+                     <Divider />
+                     <div>SUBTOTAL: {currency} {cartTotal}</div>
+                    <Button variant='contained' style={styles.button} onClick={handleClose}>PROCEED TO CHECKOUT</Button>
+                </Drawer></Box>
             </Grid>
             </Box>
         </div>
